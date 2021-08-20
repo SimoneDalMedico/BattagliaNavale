9734e3b4e40c15c4b30fdcdaba8bd6add81cc4a5
GabrieleFilippi <gabri.fil99@gmail.com>
2021-08-20 19:08:38 +0200

    ocropoid

--- giocatore.cpp
+++ giocatore.cpp
@@ -5,34 +5,18 @@ Giocatore::Giocatore()
 {
     numeroNaviPosizionate=0;
     numero_Navi_Affondate=0;
-//    const deep_ptr<cella> assalto(NaveAssalto);
-
     cella* cella_assalto=new NaveAssalto();
     deep_ptr<cella> assalto(cella_assalto);
     navi.pushBack(assalto);
-
     cella* cella_crociere=new NaveCrociere();
     deep_ptr<cella> crociere(cella_crociere);
     navi.pushBack(crociere);
-
     cella* cella_corazzata=new NaveCorazzata();
     deep_ptr<cella> corazzata(cella_corazzata);
     navi.pushBack(corazzata);
-
     cella* cella_portaaerei=new NavePortaerei();
     deep_ptr<cella> portaaerei(cella_portaaerei);
     navi.pushBack(portaaerei);
-
-
-//    deep_ptr<cella> nave;
-//    navi.pushBack(nave);
-
-//    deep_ptr<cella> crociere=new NaveCrociere();
-//    navi.pushBack(crociere);
-//    deep_ptr<cella> corazzata=new NaveCorazzata();
-//    navi.pushBack(corazzata);
-//    deep_ptr<cella> portarei=new NavePortaerei();
-    //    navi.pushBack(portarei);
 }
 
 Giocatore::Giocatore(const Giocatore & G)
@@ -45,8 +29,4 @@ Giocatore::Giocatore(const Giocatore & G)
 void Giocatore::operator=(Giocatore pos)
 {
     navi=pos.navi;
-//    assalto=pos.assalto;
-//    crociere=pos.crociere;
-//    corazzata=pos.corazzata;
-//    portaerei=pos.portaerei;
 }
